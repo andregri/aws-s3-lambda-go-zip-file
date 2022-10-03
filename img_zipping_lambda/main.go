79f49b0996d24bfa910f52ed929ccb790465f733
Andrea Grillo <andrea.grillo96@live.com>
2022-10-03 17:55:01 +0200

    Zip the file and upload to S3 compressed/ folder

--- img_zipping_lambda/main.go
+++ img_zipping_lambda/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"archive/zip"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -31,6 +33,7 @@ func HandleRequest(ctx context.Context, s3Event events.S3Event) (string, error) 
 	if err != nil {
 		return "", fmt.Errorf("failed to create file %q, %v", filename, err)
 	}
+	defer f.Close()
 
 	// Write the contents of S3 Object to the file
 	record := s3Event.Records[0].S3
@@ -42,6 +45,59 @@ func HandleRequest(ctx context.Context, s3Event events.S3Event) (string, error) 
 		return "", fmt.Errorf("failed to download file, %v", err)
 	}
 	log.Printf("file downloaded, %d bytes\n", n)
+
+	//// Zip the file
+	// Create a buffer to write our archive to.
+	archivePath := fmt.Sprintf("/tmp/%s.zip", record.Object.Key)
+	archive, err := os.Create(archivePath)
+	if err != nil {
+		log.Printf("Error creating archive: %s", err.Error())
+		return "", fmt.Errorf("failed to create archive, %v", err)
+	}
+	defer archive.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(archive)
+
+	// Add some files to the archive.
+	w1, err := w.Create(record.Object.Key)
+	if err != nil {
+		log.Printf("Error adding file to archive: %s", err.Error())
+		return "", fmt.Errorf("failed to add file to archive, %v", err)
+	}
+
+	_, err = io.Copy(w1, f)
+	if err != nil {
+		log.Printf("Error copying file to archive: %s", err.Error())
+		return "", fmt.Errorf("failed to copying file to archive, %v", err)
+	}
+
+	// Make sure to check the error on Close.
+	if err = w.Close(); err != nil {
+		return "", fmt.Errorf("failed to copying file to archive, %v", err)
+	}
+
+	/// Upload zip to S3
+	archiveToUpload, err := os.Open(archivePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open archive, %v", err)
+	}
+
+	defer archiveToUpload.Close()
+	// Setup the S3 Upload Manager. Also see the SDK doc for the Upload Manager
+	// for more information on configuring part size, and concurrency.
+	//
+	// http://docs.aws.amazon.com/sdk-for-go/api/service/s3/s3manager/#NewUploader
+	uploader := s3manager.NewUploader(sess)
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(record.Bucket.Name),
+		Key:    aws.String(fmt.Sprintf("compressed/%s.zip", record.Object.Key)),
+		Body:   archiveToUpload,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to upload file to S3, %v", err)
+	}
+
 	return fmt.Sprintf("Hello %s!", s3Event.Records[0].EventName), nil
 }
 
